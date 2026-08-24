91164bdbb466cb534ff8e4ea0ffea237d058584d
WenderAKAD <wenndder.sp64@gmail.com>
2026-08-24 17:37:26 -0300

    1/15max100

---
+++ 1:15max100/Exercício1:100.py
@@ -0,0 +1,12 @@
+''' Escreva um programa que leia dois números inteiros e mostre a soma deles.  
+Exemplo de execução:
+Primeiro valor: 8 
+Segundo valor: 5    
+Soma: 13
+'''
+
+primeiroValor = int(input("Primeiro valor: "))
+segundoValor = int(input("Segundo valor: "))
+soma = primeiroValor + segundoValor
+print("Soma: {0}".format(soma))
+#print(f"Soma: {soma}")
