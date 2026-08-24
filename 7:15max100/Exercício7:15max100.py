ff20c0bdac6f1a41f7ef67892bb0b8dc6730c3f4
WenderAKAD <wenndder.sp64@gmail.com>
2026-08-24 17:42:18 -0300

    7/15max100

---
+++ 7:15max100/Exercício7:15max100.py
@@ -0,0 +1,6 @@
+'''. Exercício 7: Celsius para Fahrenheit - Leia uma temperatura em graus celsius e mostre o valor equivalente em Fahrenheit.
+'''
+valor1 = float(input(' Digite um valor: '))
+celsius = valor1
+fahrenheit = celsius * 1.8 + 32
+print(' {0} em graus celsius equivalem a {1} graus fahrenheit. '.format(celsius, fahrenheit))
