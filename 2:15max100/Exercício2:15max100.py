ce7f8efed102bc4ab1a3be23fd4472c0a0539e97
WenderAKAD <wenndder.sp64@gmail.com>
2026-08-24 17:38:38 -0300

    2/15max100

---
+++ 2:15max100/Exercício2:15max100.py
@@ -0,0 +1,8 @@
+''' Exercício 2 = Leia duas notas reais, calcule a média e mostre o resultado com a casa decimal. 
+'''
+
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda Nota: '))
+media = (nota1 + nota2) / 2
+print('A média das notas é: {0}'.format(media))
+#print(f'A média das notas é: {media}')
